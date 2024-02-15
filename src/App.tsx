28b2803a9caef35b69db937b7a60f1c3fe132fda
jairojuniordev <dev.jairojunior@gmail.com>
2024-02-15 15:31:43 -0300

    add loading text

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { useSinglePrismicDocument } from "@prismicio/react";
+import {
+  useAllPrismicDocumentsByType,
+  useSinglePrismicDocument,
+} from "@prismicio/react";
 import { NavBar } from "./components/NavBar";
 
 function App() {
-  const [document] = useSinglePrismicDocument("home");
-  console.log(import.meta.env.VITE_PRISMIC_REPOSITORY);
+  // const [posts, { state }] = useAllPrismicDocumentsByType("blog_post", {
+  //   orderings: {
+  //     field: "document.first_publication_date",
+  //     direction: "desc",
+  //   },
+  // });
+
+  const [document, { state }] = useSinglePrismicDocument("home");
 
   return (
     <>
       <NavBar />
-      {document?.data && (
+      {state == "loading" && (
+        <main className="flex flex-col gap-2 text-gray-300 text-center">
+          <h1 className="text-xl font-bold">Carregando...</h1>
+        </main>
+      )}
+      {state == "loaded" && document?.data && (
         <main className="flex flex-col gap-2 text-gray-300 text-center">
           <h1 className="text-xl font-bold">
             {document.data.home_title[0].text}
